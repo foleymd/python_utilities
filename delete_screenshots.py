19c6160125c2c4f48fa2f289fbf6700fad4f0313
Marjorie Foley <foleymd@gmail.com>
2020-10-01 17:26:24 -0500

    adding screenshot deletion utility

---
+++ delete_screenshots.py
@@ -0,0 +1,11 @@
+import os, glob
+
+print('Please enter the directory from which you would like to delete screenshots.')
+print('Starting from the home directory - ~ - is assumed.')
+    
+my_dir = os.path.expanduser('~')
+directory = input()
+
+path = my_dir + '/' + directory + "/Screen Shot*"
+for filename in glob.glob(path):
+    os.remove(filename) 
